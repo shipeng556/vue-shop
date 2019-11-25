491650d91ff7adc00b13a0fb7fa0b9e7432f4dca
shipeng556 <1013404682@qq.com>
2019-11-25 10:30:16 +0800

    完成了登录功能2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+
+import axios from 'axios'
+// 配置请求的根路径（登录接口）
+axios.defaults.baseURL = '#'  
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
