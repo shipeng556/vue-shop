45840b8b8be222411b97cf59d9780bf2e8cb677c
shipeng556 <1013404682@qq.com>
2019-12-02 10:27:33 +0800

    完成了用户列表功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import {
+    Button, Form, FormItem, Input, Message, Container,
+    Header, Aside, main, Menu, MenuItem, MenuItemGroup,
+    Submenu, Breadcrumb, BreadcrumbItem, Card, Row, col,
+    Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
-import Home from './components/Home.vue'
+import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/users/Users.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home', component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users }]
+  }
 ]
 
 const router = new VueRouter({
@@ -16,11 +24,10 @@ const router = new VueRouter({
 })
 // 挂载路由导航守卫
 router.beforeEach((to, from, next) => {
-
   if (to.path === '/login') return next()
   // 获取token
   const tokenStr = window.sessionStorage.getItem('token');
-  if (!token) return next('/login')
+  if (!tokenStr) return next('/login')
   next()
 })
 
