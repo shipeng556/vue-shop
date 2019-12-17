94fd6799a6eb706f40d1171f0049d31435c769c2
shipeng556 <1013404682@qq.com>
2019-12-17 15:47:21 +0800

    完成订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -8,6 +8,9 @@ import {
     TabPane, Steps, Step, CheckboxGroup, Checkbox, Upload
 } from 'element-ui'
 
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
+
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -44,6 +47,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 
 Vue.prototype.$message = Message
